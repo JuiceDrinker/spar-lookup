911e3bd510af9fabfa1e371c8a8c2afda0654ce2
Adi <subramanianaditya42@gmail.com>
2023-03-03 18:56:27 +0100

    Implement POST /lookup-request

--- app.ts
+++ app.ts
@@ -1,9 +1,35 @@
 import express from "express";
+import { sparClient } from "./clients/sparClient";
+import { executeLookup } from "./services/sparLookupService";
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.post("/lookup-request", async (req, res) => {
+  const { pnr = "" } = req.body;
+  if (!pnr) {
+    return res.status(400).send("No personal identity number provided");
+  }
+
+  try {
+    const client = sparClient();
+    const sparResponse = await executeLookup(pnr.toString(), client);
+    res.status(200).json(sparResponse);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      if (err.message === "Invalid personal identity number") {
+        return res
+          .status(422)
+          .send("Invalid format of personal identity number");
+      }
+
+      if (err.message === "Communication error with SPAR service") {
+        return res.status(422).send("Could not communicate with SPAR service");
+      }
+    }
+  }
 });
 
 app.listen(port, () => {
--- services/sparLookupService.ts
+++ services/sparLookupService.ts
@@ -3,15 +3,23 @@ import parser from "xml2json";
 
 export const executeLookup = async (
   pnr: string,
-  client: { makeLookupRequest: (pnr: string) => Promise<unknown> }
+  client: {
+    makeLookupRequest: (pnr: string) => Promise<{ data: unknown }>;
+  }
 ) => {
   try {
     if (isValidPnr(pnr)) {
-      return await client.makeLookupRequest(pnr);
+      const result = await client.makeLookupRequest(pnr);
+      if (typeof result.data === "string" || Buffer.isBuffer(result.data)) {
+        return parser.toJson(result.data);
+      }
     } else {
       throw new Error("Invalid personal identity number");
     }
   } catch (err) {
+    if (err.message === "Invalid personal identity number") {
+      throw err;
+    }
     throw new Error("Communication error with SPAR service");
   }
 };
